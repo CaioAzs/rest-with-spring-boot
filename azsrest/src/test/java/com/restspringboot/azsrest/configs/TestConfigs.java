0b392534a5a4f68b87bc4ef7ec45028db328f8c8
CaioAzs <souzascaio23cs@gmail.com>
2023-07-31 12:44:24 -0300

    Started Tests Implementation

--- azsrest/src/test/java/com/restspringboot/azsrest/configs/TestConfigs.java
+++ azsrest/src/test/java/com/restspringboot/azsrest/configs/TestConfigs.java
@@ -1,6 +1,13 @@
 package com.restspringboot.azsrest.configs;
 
 public class TestConfigs {
-
+	//Tests Server Port
 	public static final int SERVER_PORT = 8888;
+	public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+	public static final String HEADER_PARAM_ORIGIN = "Origin";
+	
+	public static final String CONTENT_TYPE_JSON = "application/json";
+	public static final String CONTENT_TYPE_XML = "application/xml";
+
+	public static final String ORIGIN_CAIO = "https://github.com/CaioAzs";
 }
