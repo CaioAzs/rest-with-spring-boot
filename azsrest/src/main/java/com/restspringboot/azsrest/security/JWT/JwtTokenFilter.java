29419fca9402a9c4e6a9db39743733dc15e9dc84
CaioAzs <souzascaio23cs@gmail.com>
2023-08-19 13:38:40 -0300

    Added download and upload support

--- azsrest/src/main/java/com/restspringboot/azsrest/security/JWT/JwtTokenFilter.java
+++ azsrest/src/main/java/com/restspringboot/azsrest/security/JWT/JwtTokenFilter.java
@@ -7,31 +7,43 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class JwtTokenFilter extends GenericFilterBean{
 
 	@Autowired
-	private JwtTokenProvider tokenProvider;
-	
-	public JwtTokenFilter(JwtTokenProvider tokenProvider) {
-		this.tokenProvider = tokenProvider;
-	}
+	private final JwtTokenProvider tokenProvider;
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		String token = tokenProvider.resolveToken((HttpServletRequest) request);
-		if (token != null && tokenProvider.validateToken(token)) {
-			Authentication auth = tokenProvider.getAuthentication(token);
-			if (auth != null) {
-				SecurityContextHolder.getContext().setAuthentication(auth);
-			}
-		}
-		chain.doFilter(request, response);		
-	}
+    public JwtTokenFilter(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        try {
+            String token = tokenProvider.resolveToken((HttpServletRequest) request);
+            if (token != null && tokenProvider.validateToken(token)) {
+                Authentication auth = tokenProvider.getAuthentication(token);
+                if (auth != null) {
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            }
+        } catch (TokenExpiredException ex) {
+            // Tratamento do TOKEN EXPIRADO
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            httpResponse.getWriter().write("Token expired: " + ex.getMessage());
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
 }
